9e4b11721f7de4776140db8cd503a233b609a330
Christian Peterson <cpeterson3@nic.edu>
2024-05-07 10:43:00 -0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,10 @@ const About = () => {
     return (
       <div>
         <h1>About Us</h1>
-        <p>Welcome to our About page. Here you can learn more about our company or organization.</p>
+        <p>Welcome to our About page. Here you can learn more about our company or organization.
+        </p>
+         <p> While this was a very complicated class for me, I'm very grateful to have this oppurtunity to learn how to code. The Vercel was probably my easiest aspeect that we learned, but I really enjoyed Vanilla Javascript and doing the Todo app as well. I can't wait to continue learning next semester. </p>
+        
         
           <footer>
       &copy; Copyright 2024 GDES 255 Final
