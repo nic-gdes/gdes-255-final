cb1a27dab4a47cf628110c06c9ff0c68afedbe76
Christian Peterson <cpeterson3@nic.edu>
2024-05-07 10:32:15 -0700

    Final Project

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/global/header";
+import styles from "./page.module.css";
+
+const About = () => {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>Welcome to our About page. Here you can learn more about our company or organization.</p>
+        
+          <footer>
+      &copy; Copyright 2024 GDES 255 Final
+    </footer>       
+      </div>
+    
+    );
+  }
+  
+  export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-//import Header from "@/components/global/header";
+import Header from "@/components/global/header";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -6,25 +6,23 @@ export default function Home() {
     <>
       <Header />
       <main className={styles.main}>
-        {/*These boxes needs to be center of page each one 300x250px size with a margin of 15px and 20px padding.*/}
         <div className={styles.container}>
           <div className={styles.row}>
             <div className={styles.boxLeft}>
-              {/*This box needs to be purple*/}
+                                                                                                                                                                                                                                                                                              
             </div>
             <div className={styles.boxCenter}>
-              {/*This box needs to be blue*/}
+           
             </div>
             <div className={styles.boxRight}>
-              {/*This box needs to be red*/}
+              
             </div>
           </div>
         </div>
       </main>
       <footer>
-        {/*This footer needs to have the copyright symbol and needs to be a global component similar to the header. */}
-        {/*The main body needs to grow so the footer is at bottom of screen.*/}
-        Copyright 2024 GDES 255 Final
+       
+        &copy; Copyright 2024 GDES 255 Final
       </footer>
     </>
   );
