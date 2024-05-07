4f77de7105b469e1d1c3ca43b0a7b249e3402558
Laurel Miller <stabbatkennels@gmail.com>
2024-05-07 12:45:58 -0700

    Adding boxes, footer, ect

---
+++ src/app/About.tsx
@@ -0,0 +1,12 @@
+
+const AboutPage = () => {
+  return (
+      <div>
+        <h1>About Me</h1>
+        <p>
+        </p>
+      </div>
+  );
+};
+
+export default AboutPage;
--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -4,33 +4,37 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className={styles.header}>
-
+      
       {/*This navigation needs to be stuck to the top of the screen when you scroll.*/}
       {/*The header needs to be white and the links need to be in a row.*/}
       <div className={styles.logo}>
         {/* Image needs to be the NIC logo and needs to be linked to the home page of this app. */}
+        <Link href="/">
         <Image 
-        src="./NIClogo.png"
+        src="/NIClogo.png"
         alt="NIC Logo"
         height="100"
-        width="400"/>
-        <Link href="/">
-          <img src="./NIClogo.png" alt="Homepage" />
-    </Link>
+        width="300"
+        bg-color="white"/>
+        </Link>
       </div>
       <nav>
-        <ul>
+        <div className={styles['nav-container']}>
+        <ul className={styles['nav-list']}>
           <li>
             {/* Link to homepage */}
-    <Link href="/">
-              <a>Home</a>
+            <Link href="/">
+            Home
             </Link>
           </li>
           <li>
             {/*Link to the about me page that you have to create.*/}
-            About
+            <Link href= "/app/About.tsx">
+              About
+            </Link>
           </li>
         </ul>
+        </div>
       </nav>
     </header>
   );
